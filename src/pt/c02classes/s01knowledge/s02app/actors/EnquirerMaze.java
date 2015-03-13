14c3be864dd4daad812feb325e81eac9e5defcd1
Gabriel-Siqueira <gabriel._gabriel@hotmail.com>
2015-03-12 23:47:47 -0300

    maze certo

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -9,7 +9,7 @@ import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 public class EnquirerMaze implements IEnquirer {
 
 	IResponder responder;
-	Hashtable<Integer,HashSet<Integer>> vizitado = new Hashtable<Integer,HashSet<Integer>>();
+	Hashtable<Integer,HashSet<Integer>> visitado = new Hashtable<Integer,HashSet<Integer>>();
 	
 	public void connect(IResponder responder) {
 		this.responder = responder;
@@ -17,11 +17,11 @@ public class EnquirerMaze implements IEnquirer {
 	
 	public boolean acha_saida(String direcao, int x, int y){
 		HashSet<Integer> visitados_linha;
-		if(!vizitado.containsKey(y)){
+		if(!visitado.containsKey(y)){
 			visitados_linha = new HashSet<Integer>();
-			vizitado.put(y, visitados_linha);
+			visitado.put(y, visitados_linha);
 		}else{
-			visitados_linha = vizitado.get(y);
+			visitados_linha = visitado.get(y);
 			if(visitados_linha.contains(x)) return false;
 		}
 		visitados_linha.add(x);
@@ -30,7 +30,7 @@ public class EnquirerMaze implements IEnquirer {
 			case "parede": return false;
 			case "entrada": return false;
 			case "passagem": if(!responder.move(direcao)) return false; break;
-			case "saida": return true;
+			case "saida": responder.move(direcao); return true;
 		}
 		
 		if(direcao != "sul") 
